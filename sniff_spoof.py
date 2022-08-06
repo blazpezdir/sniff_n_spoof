7494337f4b4493cc719b801030b607c036f7c5ac
blazpezdir <pezdir.blaz@gmail.com>
2022-08-06 23:27:50 +0200

    first commit

--- sniff_spoof.py
+++ sniff_spoof.py
@@ -5,6 +5,8 @@ def spoof_reply(pkt):
     a = IP()
     a.dst = pkt['IP'].src
     a.src = pkt['IP'].dst
+    # We recognize a successful spoof in wireshark by looking if the ttl matches
+    a.ttl = 111
 
     b = ICMP()
     b.type = 0
